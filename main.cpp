a567e4ce5d68f81c09b4f89d0f6f8314779c7d49
Salabrat <salabrat66@gmail.com>
2023-05-30 15:28:23 +0400

    NEWDESIGN

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Применение стилей Qt
+    QApplication::setStyle("Fusion");
+
+    // Настройка цветовой схемы
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor("#f0f0f0")); // Цвет фона окна
+    palette.setColor(QPalette::WindowText, QColor("#44444")); // Цвет текста
+    palette.setColor(QPalette::Button, QColor("#dddddd")); // Цвет кнопок
+    palette.setColor(QPalette::ButtonText, QColor("#333333")); // Цвет текста кнопок
+    a.setPalette(palette);
+
     MainWindow w;
     w.show();
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,16 +6,19 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    setWindowTitle("Генератор случайных чисел");
+    setFixedSize(300, 200);
+
     QPushButton *button = new QPushButton("Сгенерировать случайное число", this);
+    button->setGeometry(50, 50, 200, 30);
+
     connect(button, &QPushButton::clicked, [=]() {
         QTime time = QTime::currentTime();
         qsrand(static_cast<uint>(time.msec()));
 
-        int randomNumber = qrand() % 1912111 + 1;
+        int randomNumber = qrand() % 100 + 1;
         QMessageBox::information(this, "Случайное число", "Сгенерировано случайное число: " + QString::number(randomNumber));
     });
-
-    setCentralWidget(button);
 }
 
 MainWindow::~MainWindow()
